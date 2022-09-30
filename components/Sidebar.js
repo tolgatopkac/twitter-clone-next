9bf8d907fbf38e8f3bb83998613a4b64fbbb328b
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-09-30 07:25:18 +0300

    create sidebar component with tailwind css

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,9 +13,9 @@ import {
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
       {/* Twitter Logo */}
-      <div className="">
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image
           width="50"
           height="50"
@@ -25,8 +25,8 @@ const Sidebar = () => {
 
       {/* Menu */}
 
-      <div className="">
-        <SidebarMenuItem text="Home" Icon={HomeIcon} />
+      <div className="mt-4 mb-2.5 xl:item-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
         <SidebarMenuItem text="Notification" Icon={BellIcon} />
         <SidebarMenuItem text="Message" Icon={InboxIcon} />
@@ -37,20 +37,22 @@ const Sidebar = () => {
       </div>
       {/* Button */}
 
-      <button>Tweet</button>
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+        Tweet
+      </button>
       {/* Mini-Profile */}
 
-      <div className="">
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
         <img
           src="https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/4:3/w_3332,h_2499,c_limit/1521-WIRED-Cat.jpeg"
           alt="User Image"
-          className="h-24 w-24 rounded-full"
+          className="h-10 w-10 rounded-full xl:mr-2"
         />
-        <div className="">
-          <h4>Tolga Topkaç</h4>
-          <p>@tolgatopkac</p>
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Tolga Topkaç</h4>
+          <p className="text-gray-500">@tolgatopkac</p>
         </div>
-        <EllipsisHorizontalIcon className="h-5" />
+        <EllipsisHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
       </div>
     </div>
   );
