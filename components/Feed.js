7435bc0f6f59ef3d936778999e35e0fbaeb07b23
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-10-01 05:56:05 +0300

    add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,30 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 const Feed = () => {
+  const posts = [
+    {
+      id: "1",
+      name: "Tolga Topkaç",
+      username: "tolgatopkac",
+      userImg:
+        "https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/4:3/w_3332,h_2499,c_limit/1521-WIRED-Cat.jpeg",
+      img: "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Tolga Topkaç",
+      username: "tolgatopkac",
+      userImg:
+        "https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/4:3/w_3332,h_2499,c_limit/1521-WIRED-Cat.jpeg",
+      img: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1274&q=80",
+      text: "Woww!",
+      timestamp: "2 days ago",
+    },
+  ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +34,9 @@ const Feed = () => {
         </div>
       </div>
       <Input />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 };
