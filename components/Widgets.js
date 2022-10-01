4716812012ba8df8529715994bfbe401f8144921
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-10-01 18:27:09 +0300

    create the news section of the widgets component

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,9 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import News from "./News";
 
-const Widgets = () => {
+const Widgets = ({ newsResults }) => {
+  const [articleNum, setArticleNum] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -12,6 +15,18 @@ const Widgets = () => {
             placeholder="Search Twitter"
           />
         </div>
+      </div>
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] ">
+        <h4 className="font-bold text-xl px-4">What's happening</h4>
+        {newsResults.slice(0, articleNum).map((article) => (
+          <News key={article.title} article={article} />
+        ))}
+        <button
+          onClick={() => setArticleNum(articleNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
+          Show more
+        </button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home() {
+export default function Home({ newsResults }) {
   return (
     <div>
       <Head>
@@ -12,16 +12,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen max-w-7xl mx-auto ">
+      <main className="flex min-h-screen  mx-auto ">
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
         <Feed />
         {/* Widgets */}
-        <Widgets />
+        <Widgets newsResults={newsResults.articles} />
 
         {/* Sidebar */}
       </main>
     </div>
   );
+}
+
+// https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json
+
+// fetch api SSR
+export async function getServerSideProps() {
+  const newsResults = await fetch(
+    "https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json"
+  ).then((res) => res.json());
+  return {
+    props: {
+      newsResults,
+    },
+  };
 }
